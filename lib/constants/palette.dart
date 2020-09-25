d1f43385dc8edf2760f6f8575ccf306472deae39
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-09-25 22:12:35 +0700

    feat(palette): change background color

--- lib/constants/palette.dart
+++ lib/constants/palette.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const Color primaryColor = Color(0xFF073BC8);
-  static const Color backgroundColor = Color(0xFFF6FBFF);
+  static const Color backgroundColor = Color(0xFFFBFEFF);
 }
