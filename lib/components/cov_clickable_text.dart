2d7256c2c5b2838b6d1c6e46120d46977c0797d6
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-09-26 01:41:51 +0700

    feat(components): create clickable text

---
+++ lib/components/cov_clickable_text.dart
@@ -0,0 +1,34 @@
+import 'package:covidfo/components/cov_text.dart';
+import 'package:covidfo/constants/palette.dart';
+import 'package:flutter/material.dart';
+
+
+class CovClickableText extends StatelessWidget {
+
+  CovClickableText({
+    @required this.onPressed,
+    @required this.text,
+    this.textColor = Colors.white
+  });
+
+  final Function onPressed;
+  final String text;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      splashColor: Colors.blue[100],
+      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+      child: CovText(
+        textContent: text,
+        textAlign: TextAlign.center,
+        fontFamily: 'Roboto',
+        fontSize: 14.0,
+        fontWeight: FontWeight.w400,
+        textColor: textColor,
+      ),
+    );
+  }
+}
