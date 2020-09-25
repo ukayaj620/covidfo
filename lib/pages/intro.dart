863f70f440a2273b2fea9bd18bf53872cbd8b15e
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-09-25 22:49:09 +0700

    feat: finish layout of intro page

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -1,3 +1,4 @@
+import 'package:covidfo/components/cov_button.dart';
 import 'package:flutter/material.dart';
 import 'package:covidfo/constants/palette.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,46 +9,59 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          color: Palette.backgroundColor,
-        ),
-        padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width * 0.1,
-          vertical: MediaQuery.of(context).size.height * 0.1,
-        ),
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20.0),
-              child: SvgPicture.asset(
-                "assets/images/covid19_new.svg",
-                width: MediaQuery.of(context).size.width * 0.8,
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: Palette.backgroundColor,
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.1,
+            vertical: MediaQuery.of(context).size.height * 0.05,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                child: SvgPicture.asset(
+                  "assets/images/covid19_new.svg",
+                  width: MediaQuery.of(context).size.width * 0.75,
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8.0),
-              child: CovText(
-                textContent: 'Keep your COVID-19 Information up to date',
-                textAlign: TextAlign.center,
-                fontFamily: 'Quicksand',
-                fontSize: 24.0,
-                fontWeight: FontWeight.w900,
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8.0),
+                child: CovText(
+                  textContent: 'Keep your COVID-19 Information up to date',
+                  textAlign: TextAlign.center,
+                  fontFamily: 'Quicksand',
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w900,
+                  textColor: Colors.black87,
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8.0),
-              child: CovText(
-                textContent: 'We provide a real time data of COVID-19 all around the world',
-                textAlign: TextAlign.center,
-                fontFamily: 'Roboto',
-                fontSize: 16.0,
-                fontWeight: FontWeight.w300,
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8.0),
+                child: CovText(
+                  textContent: 'We provide a real time data of COVID-19 all around the world',
+                  textAlign: TextAlign.center,
+                  fontFamily: 'Roboto',
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w300,
+                  textColor: Colors.black87,
+                ),
               ),
-            ),
-          ],
+              Container(
+                alignment: Alignment.bottomCenter,
+                margin: EdgeInsets.symmetric(vertical: 4.0),
+                child: CovButton(
+                  onPressed: () => print('Fly to sign up page'),
+                  color: Palette.primaryColor,
+                  buttonText: 'Get Started',
+                ),
+              ),
+            ],
+          )
         )
       )
     );
