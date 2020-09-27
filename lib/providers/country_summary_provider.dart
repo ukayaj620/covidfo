75c34dd4417dec5f8dde632a94a6256d0a191b30
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-09-28 01:14:15 +0700

    feat: add function to get list of information of every single country

--- lib/providers/country_summary_provider.dart
+++ lib/providers/country_summary_provider.dart
@@ -1,4 +1,5 @@
 import 'package:covidfo/models/country_summary_model.dart';
+import 'package:covidfo/models/single_country_summary_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -11,6 +12,18 @@ class CountrySummaryProvider {
 
     if (response.statusCode == 200) {
       return new CountrySummaryModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception();
+    }
+  }
+
+  Future<List<SingleCountrySummaryModel>> fetchCountriesDetailsSummary() async {
+    final response = await http.Client().get(_url, headers: {"Accept": "application/json"});
+
+    if (response.statusCode == 200) {
+      List<SingleCountrySummaryModel> countries =  (json.decode(response.body) as List).map((country) => new SingleCountrySummaryModel.fromJson(country)).toList();
+      print(countries);
+      return countries;
     } else {
       throw Exception();
     }
