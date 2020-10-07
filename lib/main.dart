3b1201de35017bba80c517036e9e15be92f6069d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-10-07 23:46:50 +0700

    feat: set false show debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Covidfo',
       initialRoute: Splash.id,
       routes: {
